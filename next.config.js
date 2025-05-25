db795aeaa584c3f82760d7fd945ab9a199e8f5d5
chongmatthew37 <chongmatthew37@gmail.com>
2025-05-24 23:28:58 -0400

    styling fixes

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  assetPrefix: './',
+  distDir: 'dist',
+  trailingSlash: true,
 }
 
 module.exports = nextConfig 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -24,6 +24,7 @@ const config: Config = {
     },
   },
   plugins: [],
+  darkMode: 'class',
 }
 
 export default config;
